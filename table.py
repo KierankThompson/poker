11e40348c6e8aa4838f8e354120ac4efe0427a41
KierankThompson <thompson.kierank@gmail.com>
2025-03-31 23:03:18 -0400

    fixed table.py

--- table.py
+++ table.py
@@ -1,6 +1,25 @@
-from card import Card
+from card import Card, Deck
 
 def table():
     def __init__(self):
         self.cards = []
-        
+        self.pot = 0
+    
+    def addPot(self,bet):
+        self.pot += bet
+    
+    def flop(self,deck):
+        cards = deck.takeout(3)
+        for card in cards:
+            self.cards.append(card)
+    
+    def turn(self,deck):
+        card = deck.takeout(1)
+        self.cards.append(card)
+    
+    def river(self,deck):
+        turn(deck)
+
+    def reset(self):
+        self.cards = []
+        self.pot = 0

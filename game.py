35794f6a8a34b41137596dd188f5d8b273e71c68
KierankThompson <thompson.kierank@gmail.com>
2025-04-03 19:44:33 -0400

    fixed lookup

--- game.py
+++ game.py
@@ -1,24 +1,76 @@
 from card import Card,Deck
 from table import Table
+from lookup import *
+from player import Player, AI
+
+
+def find_fast(x):
+    x += 0xe91aaa35
+    x ^= x >> 16
+    x += x << 8
+    x &= 0xffffffff
+    x ^= x >> 4
+    b = (x >> 8) & 0x1ff
+    a = (x + (x << 2)) >> 19
+    r = (a ^ hash_adjust[b]) & 0x1fff
+    return hash_values[r]
 
 
 def evalHand(cards):
     if len(cards) != 5:
         raise Exception("Hand Must Have 5 Cards")
-    return cards[0].bit & cards[1].bit & cards[2].bit & cards[3].bit & cards[4].bit & 0xF000
+    q = (cards[0].bit | cards[1].bit | cards[2].bit | cards[3].bit | cards[4].bit) >> 16
+    s = 0
+    if (cards[0].bit & cards[1].bit & cards[2].bit & cards[3].bit & cards[4].bit & 0xf000):
+        return flushes[q]
+    if ((s := unique5[q])):
+        return s
+    q = (cards[0].bit & 0xff) * (cards[1].bit & 0xff) * (cards[2].bit & 0xff) * (cards[3].bit & 0xff) * (cards[4].bit & 0xff)
+    return find_fast(q)
+
+def handRank(value):
+    if (value > 6185):
+        return "High Card"   
+    if (value > 3325): 
+        return "One Pair"       
+    if (value > 2467): 
+        return "Two Pair"       
+    if (value > 1609): 
+        return "Three of a Kind"  
+    if (value > 1599): 
+        return "Straight"      
+    if (value > 322): 
+        return "Flush"         
+    if (value > 166):
+        return "Full House"       
+    if (value > 10):   
+        return "Four of a Kind"  
+    return "Straight Flush"  
     
 
 
 def main():
-    d = Deck()
-    cards = []
-    result = 0
-    while result == 0:
-        cards = d.takeOut(5)
-        d.reset()
-        result = evalHand(cards)
-    print(result)
-    print(cards)
+    table = Table()
+    deck = Deck()
+    human = Player(1000)
+    ai = AI(1000)
+    turn = 1
+    while True:
+        #Blind
+        if turn % 2:
+            human.bet(5)
+        else:
+            ai.bet(5)
+        table.addPot(5)
+
+        human.getHand(deck)
+        ai.getHand(deck)
+        break
+
+
+
+
+
     
     
 
--- player.py
+++ player.py
@@ -1,7 +1,10 @@
 from card import Deck
+import random
+import numpy as np
+
 
 class Player():
-    def __init__(self,startingMoney):
+    def __init__(self,startingMoney=1000):
         self.money = startingMoney
         self.hand = []
 
@@ -12,10 +15,44 @@ class Player():
         self.money += potSize
     
     def getHand(self,deck):
-        self.hand = deck.takeout(2)
+        self.hand = deck.takeOut(2)
 
     
     
     def reset(self):
         self.hand = []
+
+class AI(Player):
+    
+
+    def __init__(self,startingMoney=1000):
+        super().__init__(startingMoney)
+
+    def evalPreFlop(self, isBlind = True, curBet = 0):
+        if not self.hand:
+            return 0
+        maxScore = 41 ** 2
+        score = maxScore
+        if (self.hand[0].bit & self.hand[1].bit & 0xf000):
+            score = maxScore
+        else:
+            score = (self.hand[0].bit & 0xff) * (self.hand[1].bit & 0xff)
+        if isBlind:
+            ratio = 1 - (score/maxScore-0.3)
+            rand = random.randrange(1)
+            if ratio > rand:
+                return 5 + min(self.money,int(self.money * np.random.normal(0.1)))
+            else:
+                return 5
+        else:
+            ratio = score/maxScore - 0.1
+            
+        
+        
+        
+        
+
+
+
+
 
